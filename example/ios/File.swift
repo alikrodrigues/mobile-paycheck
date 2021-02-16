6674d989d5a062c56f63d3381c1f80053167f170
Alik Rodrigues <alik@nexti.com>
2021-02-16 20:36:14 -0300

    Init.

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  MobPaycheckExample
-//
-
-import Foundation
---
+++ src/componentes/ListView/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+export default ListView = () => {
+    
+
+    return (
+        <View style={{backgroundColor: 'red', padding: 10}}>
+            <Text>List View Modulo do contracheque</Text>
+        </View>
+    )
+
+
+}
